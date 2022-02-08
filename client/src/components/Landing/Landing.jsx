21bd4dcbd5d1f2d322ba4c4b19ea43a391b9c253
Guido Ortiz <guidoortiz93@hotmail.com>
2022-02-08 06:29:14 -0300

    landing styles

--- client/src/components/Landing/Landing.jsx
+++ client/src/components/Landing/Landing.jsx
@@ -1,13 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import s from './Landing.module.css';
+import img from './assets/landing.jpg';
+import img1 from './assets/download.jpg';
 
 export default function Landing(){
     return(
-        <div>
-            <h1>HOLA, esta es mi landing</h1>
-            <Link to = '/home'>
-                <button>Ingresar</button>
-            </Link>
+        <div className={`${s.fondo} ${s.container}`}>
+            <div className={s.flex}>
+                <h1 className={s.titulo}>RAWG Video Games</h1>
+                
+                <Link to = '/home'>
+                    <button className={s.btn}>Get Started</button>
+                </Link>
+            </div>
+            
+            {/* <img src={img1} className={s.imagen} alt='img not found' /> */}
+            
             
         </div>
     )
